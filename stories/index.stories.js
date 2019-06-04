6c7161b3ce8d36fc01428eec3eb7901cfcda54f1
mpppk <niboshiporipori@gmail.com>
2019-06-04 16:41:22 +0900

    Remove unused stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
 import { WoolfStatView } from '../src/components/WoolfStatView';
 import { WoolfView } from '../src/components/WoolfView';
 import {
@@ -13,25 +10,6 @@ import {
   jobStatForWoolfStatViewFunctionSelected,
   stats
 } from './data';
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with text2', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
 
 storiesOf('WoolfStatView', module)
   .add('function selected', () => {
