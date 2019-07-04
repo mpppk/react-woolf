55b6fb8b6ecf0729887a16dc1199172657b43ad2
mpppk <niboshiporipori@gmail.com>
2019-07-05 08:38:42 +0900

    Add event and results rows to WoolfStatView

--- stories/data.js
+++ stories/data.js
@@ -1,5 +1,7 @@
 export const jobStat = {
   environment: 'pending',
+  event: { count: 0 },
+  results: { count: 2 },
   fromJobIDs: [],
   funcs: [
     {
@@ -27,12 +29,16 @@ export const funcStat = {
   Role: '-',
   Runtime: 'nodejs8.10',
   FunctionName: 'test-func01',
-  state: 'DONE'
+  state: 'DONE',
+  event: { count: 0 },
+  results: { count: 2 }
 };
 
 export const stats = [
   {
     environment: 'local',
+    event: { count: 0 },
+    results: { count: 2 },
     fromJobIDs: [],
     funcs: [
       {
@@ -52,10 +58,14 @@ export const stats = [
     fromJobIDs: [0],
     funcs: [
       {
+        event: { count: 0 },
+        results: { count: 2 },
         FunctionName: 'aws-func01',
         state: 'PROCESSING'
       },
       {
+        event: { count: 0 },
+        results: { count: 2 },
         FunctionName: 'aws-func02',
         state: 'READY'
       }
@@ -69,9 +79,13 @@ export const stats = [
   },
   {
     environment: 'local',
+    event: { count: 0 },
+    results: { count: 2 },
     fromJobIDs: [0],
     funcs: [
       {
+        event: { count: 0 },
+        results: { count: 2 },
         FunctionName: 'local-func01',
         state: 'PROCESSING'
       }
@@ -85,9 +99,13 @@ export const stats = [
   },
   {
     environment: 'pending',
+    event: { count: 0 },
+    results: { count: 2 },
     fromJobIDs: [1, 2],
     funcs: [
       {
+        event: { count: 0 },
+        results: { count: 2 },
         FunctionName: 'another-func01',
         state: 'READY'
       }
@@ -103,9 +121,13 @@ export const stats = [
 
 export const jobStatForWoolfStatViewFunctionSelected = {
   environment: 'local',
+  event: { count: 0 },
+  results: { count: 1 },
   fromJobIDs: [],
   funcs: [
     {
+      event: { count: 0 },
+      results: { count: 1 },
       FunctionName: 'some-func01',
       state: 'DONE'
     }
